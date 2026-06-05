aed67d082ccc85f326c27890f76aee50d486cd6a
PleshkovaAnastasia1992 <nastya.tsvetkova@rambler.ru>
2026-06-05 17:32:43 +0500

    Decompose layout into React components

---
+++ src/components/Column/Column.jsx
@@ -0,0 +1,16 @@
+import { Card } from "../Card/Card";
+
+export function Column({ title, tasks }) {
+    return (
+        <div className="main__column column">
+                <div className="column__title">
+                  <p>{title}</p>
+                </div>
+                <div className="cards">
+                  {tasks.map((task) => (
+                    <Card key={task.id} task={task} />
+                  ))}
+                </div>
+              </div>
+    )
+}
---
+++ src/data/tasks.js
@@ -0,0 +1,37 @@
+export const tasks = [
+  {
+    id: 1,
+    topic: 'Web Design',
+    topicClass: '_orange',
+    title: 'Название задачи',
+    date: '30.10.23',
+  },
+  {
+    id: 2,
+    topic: 'Research',
+    topicClass: '_green',
+    title: 'Название задачи',
+    date: '30.10.23',
+  },
+  {
+    id: 3,
+    topic: 'Web Design',
+    topicClass: '_orange',
+    title: 'Название задачи',
+    date: '30.10.23',
+  },
+  {
+    id: 4,
+    topic: 'Copywriting',
+    topicClass: '_purple',
+    title: 'Название задачи',
+    date: '30.10.23',
+  },
+  {
+    id: 5,
+    topic: 'Web Design',
+    topicClass: '_orange',
+    title: 'Название задачи',
+    date: '30.10.23',
+  },
+]
